33fdc4e9bd282cf690bfb6b7e4b6baf656bb96f5
Jake Berg <jberg@dminc.com>
2019-01-22 16:12:19 -0500

    Created MVC structure, added controllers, moved models and routes to separate files, added book details endpoint

---
+++ routes/users.route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const users_controller = require('../controllers/users.controller');
+
+router.post('/signup', users_controller.signup);
+router.post('/login', users_controller.login);
+
+module.exports = router;
